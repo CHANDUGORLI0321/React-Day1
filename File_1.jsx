76aaadbaf70ccbf0369cc9fd168f8232dc5a625f
CHANDUGORLI0321 <chandugorli221@gmail.com>
2025-06-12 08:29:12 +0530

    Day1

---
+++ File_1.jsx
@@ -0,0 +1,11 @@
+   
+   import  React from 'react';
+   function File_1() {
+    return(
+        <div>
+        <h1>chandu praba 221 file1</h1>
+        
+        </div>
+    )
+}
+export default File_1;
---
+++ File_2.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import File_1 from './File_1';
+
+
+export function File_2() {
+    return (
+        <div>
+            {console.log("kkd")}
+            <h6>I am Chandu Praba File_2</h6>
+            <File_1 />
+        </div>
+    );
+}
+
+// export default File_2;
